8dada4830be16e4f069200c6bc34d47e67cbe73f
Irem Nur Gozudok <iremgozudok99@icloud.com>
2025-11-06 14:10:58 +0300

    socket connection implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,8 +6,8 @@ import path from "path";
 import { connectDB } from "./lib/db.js";
 import { ENV } from "./lib/env.js";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
-const app = express();
 const __dirname = path.resolve();
 
 const PORT = ENV.PORT || 3000;
@@ -26,7 +26,7 @@ if (ENV.NODE_ENV === "production") {
   );
 }
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("Server is running on port: " + PORT);
   connectDB();
 });
